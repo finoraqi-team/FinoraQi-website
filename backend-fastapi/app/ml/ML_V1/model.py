45951e10419992109bd736e34609f884334e31b0
FinoraQi - Core Banking BRL <finoraqi.finance@gmail.com>
2026-04-05 13:05:57 -0300

    Update model.py

--- backend-fastapi/app/ml/ML_V1/model.py
+++ backend-fastapi/app/ml/ML_V1/model.py
@@ -1,12 +1,33 @@
-import time
+# import time
 
-class Model:
+# class Model:
+#     def __init__(self):
+#         print("Loading model...")
+#         time.sleep(2) # load
+
+#     def predict(self, x):
+#         time.sleep(0.5)  # inferência
+#         return {"result": x * 2}
+
+# model = Model()
+
+import joblib
+import numpy as np
+
+class RiskModel:
     def __init__(self):
-        print("Loading model...")
-        time.sleep(2) # load
+        print("Loading risk model...")
+        self.model = joblib.load("risk_model.joblib")
 
-    def predict(self, x):
-        time.sleep(0.5)  # inferência
-        return {"result": x * 2}
+    def predict(self, renda, idade, score_credito, dividas):
+        X = np.array([[renda, idade, score_credito, dividas]])
 
-model = Model()
+        prob = self.model.predict_proba(X)[0][1]  # prob de inadimplência
+        prediction = int(prob > 0.5)
+
+        return {
+            "inadimplente": prediction,
+            "probabilidade": float(prob)
+        }
+
+model = RiskModel()
